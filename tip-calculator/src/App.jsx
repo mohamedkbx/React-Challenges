2f4c4f9e9d5cc4b7be2ff6abd47ff0b345433f44
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-10-15 23:51:47 +0300

    feat: reusable component

--- tip-calculator/src/App.jsx
+++ tip-calculator/src/App.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 export default function App() {
   const [selfService, setSelfService] = useState(0);
   const [friendService, setfriendService] = useState(0);
-  const [billAmount, setBillAmount] = useState(null);
+  const [billAmount, setBillAmount] = useState("");
 
   return (
     <div style={{ marginLeft: "50px", marginTop: "50px" }}>
@@ -14,9 +14,18 @@ export default function App() {
         id="billAmount"
         text="How much Was the bill"
       />
-      <SelfService selfService={selfService} setSelfService={setSelfService} />
-      <FriendService friendService={friendService} setfriendService={setfriendService} />
+      <SelfService percent={selfService} setPercent={setSelfService}>
+        <label htmlFor="selfservice">How Do you love the service </label>
+      </SelfService>
+      <SelfService percent={friendService} setPercent={setfriendService}>
+        <label htmlFor="friendservice">How Do your Friend Like the service </label>
+      </SelfService>
       <Recip selfService={selfService} friendService={friendService} billAmount={billAmount} />
+      <Reset
+        setBillAmount={setBillAmount}
+        setSelfService={setSelfService}
+        setfriendService={setfriendService}
+      />
     </div>
   );
 }
@@ -33,33 +42,15 @@ function BillAmount({ billAmount, setBillAmount, id, text }) {
   );
 }
 
-function SelfService({ selfService, setSelfService }) {
+function SelfService({ percent, setPercent, children }) {
   function handleSelfService(event) {
     setSelfService(Number(event.target.value));
   }
 
   return (
     <div>
-      <label htmlFor="selfservice">How Do you love the service </label>
-      <select id="selfservice" value={selfService} onChange={handleSelfService}>
-        <option value="0">Dissatisfied(0%)</option>
-        <option value="5">it was okay (5%)</option>
-        <option value="10">it was good (10%)</option>
-        <option value="20">Amazing! (20%)</option>
-      </select>
-    </div>
-  );
-}
-
-function FriendService({ friendService, setfriendService }) {
-  function handleFriendService(event) {
-    setfriendService(Number(event.target.value));
-  }
-
-  return (
-    <div>
-      <label htmlFor="friendservice">How Do your Friend Like the service </label>
-      <select id="friendservice" value={friendService} onChange={handleFriendService}>
+      {children}
+      <select id="selfservice" value={percent} onChange={setPercent}>
         <option value="0">Dissatisfied(0%)</option>
         <option value="5">it was okay (5%)</option>
         <option value="10">it was good (10%)</option>
@@ -77,7 +68,21 @@ function Recip({ selfService, friendService, billAmount }) {
     <div style={{ marginTop: "20px" }}>
       <h2 style={{ marginBottom: "20px" }}>Recip</h2>
 
-      <h3>You Pay:{`$${total}($${bill} + $${tip})`}</h3>
+      <h3>You Pay:{`$${total}($${bill} + $${tip} tip) `}</h3>
+    </div>
+  );
+}
+
+function Reset({ setBillAmount, setSelfService, setfriendService }) {
+  function handleReset() {
+    setBillAmount("");
+    setSelfService(0);
+    setfriendService(0);
+  }
+
+  return (
+    <div>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
